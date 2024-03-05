d7b300ee42542bd1eef196905e79ad460ffbfc67
Markus Handal Sneve <44242647+markushs@users.noreply.github.com>
2024-03-05 23:01:47 +0100

    Update setup.py
    - Updated 'setup.py' to read requirements directly from 'requirements.txt', removing the need to parse 'version.py' for install_requires. Resolves installation issues caused by parsing errors related to dependency naming conventions.

--- setup.py
+++ setup.py
@@ -1,30 +1,23 @@
 import os
 from setuptools import setup, find_packages
+
+# HARDCODED VERSION FROM PREVIOUS (NOW DEPREACATED) version.py
+VERSION = "0.1.0rc6"
+
+# Read requirements.txt and use it for the 'install_requires' parameter
+def read_requirements():
+    with open('requirements.txt', 'r') as file:
+        return [line.strip() for line in file.readlines() if line.strip()]
+
+# Find all packages in the current directory
 PACKAGES = find_packages()
 
-# Get version and release info, which is all stored in nideconv/version.py
-ver_file = os.path.join('nideconv', 'version.py')
-with open(ver_file) as f:
-    exec(f.read())
-
-opts = dict(name=NAME,
-            maintainer=MAINTAINER,
-            maintainer_email=MAINTAINER_EMAIL,
-            description=DESCRIPTION,
-            long_description=LONG_DESCRIPTION,
-            url=URL,
-            download_url=DOWNLOAD_URL,
-            license=LICENSE,
-            classifiers=CLASSIFIERS,
-            author=AUTHOR,
-            author_email=AUTHOR_EMAIL,
-            platforms=PLATFORMS,
+# Define options for setup
+opts = dict(name="nideconv",
             version=VERSION,
             packages=PACKAGES,
-            package_data=PACKAGE_DATA,
-            install_requires=REQUIRES,
-            requires=REQUIRES)
+            install_requires=read_requirements())
 
-
+# Run setup
 if __name__ == '__main__':
     setup(**opts)
